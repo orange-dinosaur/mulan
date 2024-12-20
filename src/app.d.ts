88b4008fc0951590ad914e14ef3ec26de52870e9
orange-dinosaur <user@randomprojects.xyz>
2024-12-20 03:16:44 +0100

    Check if user is authenticated and fetch user books in hooks

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,9 +1,15 @@
+import type { Models } from 'node-appwrite';
+import type { UserBooks } from '$lib/types';
+
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
 declare global {
 	namespace App {
 		// interface Error {}
-		// interface Locals {}
+		interface Locals {
+			user: Models.User<Models.Preferences> | undefined;
+			userBooks: UserBooks | undefined;
+		}
 		// interface PageData {}
 		// interface PageState {}
 		// interface Platform {}
---
+++ src/hooks.server.ts
@@ -0,0 +1,30 @@
+import { createSessionClientEvent } from '$lib/server/appwrite';
+import { getUserBooks } from '$lib/server/books';
+
+export async function handle({ event, resolve }) {
+	// Try to get the current user from the Auth service
+	try {
+		// Use helper function to create the Appwrite client
+		const { account } = createSessionClientEvent(event);
+		// Store the current logged in user in locals,
+		// for easy access in other routes
+		event.locals.user = await account.get();
+	} catch {
+		/* empty */
+	}
+
+	// Try to get the user's books from the database
+	if (event.locals.user) {
+		try {
+			const userBooks = await getUserBooks(event.locals.user.$id);
+			// Store the books of the current logged in user in locals,
+			// for easy access in other routes
+			event.locals.userBooks = userBooks;
+		} catch {
+			/* empty */
+		}
+	}
+
+	// Continue with the request.
+	return resolve(event);
+}
