f9a98d9711944a6bbf873b612a751f61dbb0d12d
orange-dinosaur <user@randomprojects.xyz>
2025-01-01 23:12:45 +0100

    Implement login functionality with email verification and session management

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -1,8 +1,86 @@
-import { redirect } from '@sveltejs/kit';
+import {
+	createAdminClient,
+	createSessionClientCookies,
+	SESSION_COOKIE
+} from '$lib/server/appwrite.js';
+import { fail, redirect, type Cookies } from '@sveltejs/kit';
+import type { AppwriteException } from 'node-appwrite';
 
 export async function load({ locals }) {
 	if (locals.user && locals.user.emailVerification) {
 		// If the user is logged in and is email is verified, redirect to its home page.
 		redirect(301, `/users/${locals.user.$id}`);
 	}
+}
+
+export const actions = {
+	// Action to create a new user account and session
+	// A user can successully login only if the email is verified
+	// Even if the email is not verified the session is still created,
+	// so that after the user verifies the email, they can be redirected to their account page
+	login: async ({ request, cookies }) => {
+		// Extract the form data.
+		const formData = await request.formData();
+		const email = formData.get('email')?.toString() ?? '';
+		const password = formData.get('password')?.toString() ?? '';
+
+		// Create the Appwrite client.
+		const { account } = createAdminClient();
+
+		let session;
+		try {
+			session = await account.createEmailPasswordSession(email, password);
+		} catch (error) {
+			const { code, message } = error as AppwriteException;
+
+			return fail(code ?? 500, {
+				status: code,
+				message,
+				fields: {
+					email,
+					password
+				}
+			});
+		}
+
+		// Set the session cookie with the secret
+		cookies.set(SESSION_COOKIE, session.secret, {
+			sameSite: 'strict',
+			expires: new Date(session.expire),
+			secure: true,
+			path: '/'
+		});
+
+		// check if the user is verified
+		const userIsVerified = await checkVerification(cookies);
+
+		if (!userIsVerified) {
+			return fail(401, {
+				status: 401,
+				message: 'User is not verified. Please verify your email address.',
+				fields: {
+					email,
+					password
+				}
+			});
+		}
+
+		// Redirect to the account page.
+		redirect(301, `/users/${session.userId}`);
+	}
+};
+
+async function checkVerification(cookies: Cookies) {
+	// Create the Appwrite client.
+	const { account } = createSessionClientCookies(cookies);
+
+	let userAccount;
+	try {
+		userAccount = await account.get();
+		// eslint-disable-next-line @typescript-eslint/no-unused-vars
+	} catch (error) {
+		return false;
+	}
+
+	return userAccount.emailVerification;
 }
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,16 @@
 <script>
+	import { applyAction, enhance } from '$app/forms';
 	import { PUBLIC_LANDING_IMAGE } from '$env/static/public';
 	import logo from '$lib/assets/logo.png';
+	import * as Alert from '$lib/components/ui/alert/index';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
+	import { LoaderCircle, CircleAlert } from 'lucide-svelte';
+
+	const { form } = $props();
+
+	let isLoading = $state(false);
 </script>
 
 <div class="flex h-screen w-screen items-center">
@@ -20,7 +27,25 @@
 			<a href="/"><img src={logo} alt="logo" class="h-[100px] w-auto" /></a>
 		</div>
 
-		<form class="flex w-4/5 flex-col">
+		<form
+			class="flex w-4/5 flex-col"
+			method="post"
+			action="?/login"
+			use:enhance={() => {
+				isLoading = true;
+				return async ({ result, update }) => {
+					isLoading = false;
+
+					if (result.status === 200) {
+						update();
+					} else {
+						await applyAction(result);
+					}
+
+					update();
+				};
+			}}
+		>
 			<div class="grid gap-2">
 				<div class="grid gap-1">
 					<Label class="sr-only" for="email">Email</Label>
@@ -29,9 +54,11 @@
 						name="email"
 						type="email"
 						placeholder="name@example.com"
+						value={form?.fields.email ?? ''}
 						autocapitalize="none"
 						autocomplete="email"
 						autocorrect="off"
+						disabled={isLoading}
 						class="bg-transparent outline-none focus:border-b-2 focus:border-b-primary"
 					/>
 				</div>
@@ -42,13 +69,28 @@
 						name="password"
 						type="password"
 						placeholder="*************"
+						value={form?.fields.password ?? ''}
 						autocapitalize="none"
 						autocorrect="off"
+						disabled={isLoading}
 						class="bg-transparent outline-none focus:border-b-2 focus:border-b-primary"
 					/>
 				</div>
-				<Button type="submit" class="my-8 w-48 font-bold">Login</Button>
+				<Button type="submit" class="my-8 w-48 font-bold">
+					{#if isLoading}
+						<LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+					{/if}
+					Login
+				</Button>
 			</div>
 		</form>
+
+		{#if form && form?.status != 200}
+			<Alert.Root variant="destructive" class="w-3/5">
+				<CircleAlert class="mr-2 h-4 w-4" />
+				<Alert.Title>Error</Alert.Title>
+				<Alert.Description>{form?.message}</Alert.Description>
+			</Alert.Root>
+		{/if}
 	</div>
 </div>
