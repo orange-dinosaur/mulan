09fa25c7318535c4926391101eeb3fcd78ac17df
orange-dinosaur <user@randomprojects.xyz>
2025-01-06 22:26:51 +0100

    Implement user registration with email verification and session management

--- src/routes/register/+page.server.ts
+++ src/routes/register/+page.server.ts
@@ -1,8 +1,88 @@
-import { redirect } from '@sveltejs/kit';
+import { PUBLIC_AVATAR_BASE_URL, PUBLIC_EMAIL_VERIFICATION_PATH } from '$env/static/public';
+import {
+	createAdminClient,
+	createSessionClientCookies,
+	SESSION_COOKIE
+} from '$lib/server/appwrite';
+import { fail, redirect, type Cookies } from '@sveltejs/kit';
+import { ID, type AppwriteException } from 'node-appwrite';
 
 export async function load({ locals }) {
 	if (locals.user && locals.user.emailVerification) {
 		// If the user is logged in and is email is verified, redirect to its home page.
 		redirect(301, `/users/${locals.user.$id}`);
 	}
+}
+
+export const actions = {
+	// Action to create a new user account and session
+	// A user can successully login only if the email is verified
+	// Even if the email is not verified the session is still created,
+	// so that after the user verifies the email, they can be redirected to their account page
+	default: async ({ request, cookies }) => {
+		// Extract the form data.
+		const formData = await request.formData();
+		const email = formData.get('email')?.toString() ?? '';
+		const username = formData.get('username')?.toString() ?? '';
+		const password = formData.get('password')?.toString() ?? '';
+
+		// Create the Appwrite client.
+		const { account } = createAdminClient();
+
+		let session;
+		try {
+			// Create the session using the client
+			await account.create(ID.unique(), email, password, username);
+			session = await account.createEmailPasswordSession(email, password);
+
+			// Set the session cookie with the secret
+			cookies.set(SESSION_COOKIE, session.secret, {
+				sameSite: 'strict',
+				expires: new Date(session.expire),
+				secure: true,
+				path: '/'
+			});
+
+			await updateSinglePreference(cookies, 'avatar', `${PUBLIC_AVATAR_BASE_URL}${username}`);
+
+			await createVerification(cookies);
+		} catch (error) {
+			const { code, message } = error as AppwriteException;
+
+			return fail(code ?? 500, {
+				status: code,
+				message,
+				fields: {
+					email,
+					username,
+					password
+				}
+			});
+		}
+
+		return {
+			status: 200,
+			message: 'User account created successfully',
+			fields: {
+				email: '',
+				username: '',
+				password: ''
+			}
+		};
+	}
+};
+
+// TODO
+async function updateSinglePreference(cookies: Cookies, key: string, value: string) {
+	// Create the Appwrite client.
+	const { account } = createSessionClientCookies(cookies);
+
+	await account.updatePrefs({ [key]: value });
+}
+
+async function createVerification(cookies: Cookies) {
+	// Create the Appwrite client.
+	const { account } = createSessionClientCookies(cookies);
+
+	await account.createVerification(PUBLIC_EMAIL_VERIFICATION_PATH);
 }
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,9 +1,17 @@
 <script>
+	import { applyAction, enhance } from '$app/forms';
 	import { PUBLIC_LANDING_IMAGE } from '$env/static/public';
 	import logo from '$lib/assets/logo.png';
+	import * as Alert from '$lib/components/ui/alert/index';
 	import { Button } from '$lib/components/ui/button';
 	import { Input } from '$lib/components/ui/input';
 	import { Label } from '$lib/components/ui/label';
+	import { CircleAlert, LoaderCircle } from 'lucide-svelte';
+
+	const { form } = $props();
+
+	let isLoading = $state(false);
+	let isAwaitingAuthentication = $state(false);
 </script>
 
 <div class="flex h-screen w-screen items-center">
@@ -18,48 +26,95 @@
 			<a href="/"><img src={logo} alt="logo" class="h-[100px] w-auto" /></a>
 		</div>
 
-		<form class="flex w-4/5 flex-col">
-			<div class="grid gap-2">
-				<div class="grid gap-1">
-					<Label class="sr-only" for="email">Email</Label>
-					<Input
-						id="username"
-						name="username"
-						type="text"
-						placeholder="username"
-						autocapitalize="none"
-						autocomplete="username"
-						autocorrect="off"
-						class="bg-transparent outline-none focus:border-b-2 focus:border-b-primary"
-					/>
+		<!-- Form -->
+		<form
+			method="post"
+			use:enhance={() => {
+				isLoading = true;
+				return async ({ result, update }) => {
+					isLoading = false;
+
+					if (result.status === 200) {
+						isAwaitingAuthentication = true;
+						update();
+					} else {
+						await applyAction(result);
+					}
+
+					update();
+				};
+			}}
+			class="flex w-4/5 flex-col"
+		>
+			{#if isAwaitingAuthentication}
+				<p class="mb-2 text-lg text-foreground">Account created successfully</p>
+				<p class="mb-3 text-base text-foreground">check your email to complete verification</p>
+			{:else}
+				<!-- Form Fields -->
+				<div class="grid gap-2">
+					<div class="grid gap-1">
+						<Label class="sr-only" for="username">Username</Label>
+						<Input
+							id="username"
+							name="username"
+							type="text"
+							value={form?.fields.username ?? ''}
+							placeholder="username"
+							autocapitalize="none"
+							autocomplete="username"
+							autocorrect="off"
+							disabled={isLoading}
+							class="bg-transparent outline-none focus:border-b-2 focus:border-b-primary"
+						/>
+					</div>
+					<div class="grid gap-1">
+						<Label class="sr-only" for="email">Email</Label>
+						<Input
+							id="email"
+							name="email"
+							type="email"
+							value={form?.fields.email ?? ''}
+							placeholder="name@example.com"
+							autocapitalize="none"
+							autocomplete="email"
+							autocorrect="off"
+							disabled={isLoading}
+							class="bg-transparent outline-none focus:border-b-2 focus:border-b-primary"
+						/>
+					</div>
+					<div class="grid gap-1">
+						<Label class="sr-only" for="password">Pasword</Label>
+						<Input
+							id="password"
+							name="password"
+							type="password"
+							value={form?.fields.password ?? ''}
+							placeholder="*************"
+							autocapitalize="none"
+							autocorrect="off"
+							disabled={isLoading}
+							class="bg-transparent text-lg outline-none focus:border-b-2 focus:border-b-primary"
+						/>
+					</div>
+
+					<!-- Submit button -->
+					<Button type="submit" class="my-8 w-48 font-bold"
+						>{#if isLoading}
+							<LoaderCircle class="mr-2 h-4 w-4 animate-spin" />
+						{/if}
+						Register
+					</Button>
 				</div>
-				<div class="grid gap-1">
-					<Label class="sr-only" for="email">Email</Label>
-					<Input
-						id="email"
-						name="email"
-						type="email"
-						placeholder="name@example.com"
-						autocapitalize="none"
-						autocomplete="email"
-						autocorrect="off"
-						class="bg-transparent outline-none focus:border-b-2 focus:border-b-primary"
-					/>
-				</div>
-				<div class="grid gap-1">
-					<Label class="sr-only" for="password">Pasword</Label>
-					<Input
-						id="password"
-						name="password"
-						type="password"
-						placeholder="*************"
-						autocapitalize="none"
-						autocorrect="off"
-						class="bg-transparent text-lg outline-none focus:border-b-2 focus:border-b-primary"
-					/>
-				</div>
-				<Button type="submit" class="my-8 w-48 font-bold">Register</Button>
-			</div>
+			{/if}
+
+			<!-- Error alert -->
+			{#if form && form?.status != 200}
+				<Alert.Root variant="destructive" class="w-3/5">
+					<CircleAlert class="mr-2 h-4 w-4" />
+					<Alert.Title>Error</Alert.Title>
+					<Alert.Description>{form?.message}</Alert.Description>
+				</Alert.Root>
+			{/if}
 		</form>
 	</div>
 </div>
