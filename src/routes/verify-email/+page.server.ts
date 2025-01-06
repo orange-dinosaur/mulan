b0c8ae07d1236e67fd26eb8d152d5f33e52f0810
orange-dinosaur <user@randomprojects.xyz>
2025-01-06 22:27:16 +0100

    Add email verification route and UI with success/error handling

---
+++ src/routes/verify-email/+page.server.ts
@@ -0,0 +1,37 @@
+import { createClient } from '$lib/server/appwrite';
+import { redirect } from '@sveltejs/kit';
+import type { AppwriteException } from 'node-appwrite';
+
+export async function load({ locals, url }) {
+	if (locals.user && locals.user.emailVerification) {
+		// If the user is logged in and is email is verified, redirect to its home page.
+		redirect(301, `/users/${locals.user.$id}`);
+	}
+
+	const userId = url.searchParams.get('userId')?.toString() ?? '';
+	const secret = url.searchParams.get('secret')?.toString() ?? '';
+
+	// Create the Appwrite client.
+	const { account } = createClient();
+
+	// verify user account
+	try {
+		await account.updateVerification(userId, secret);
+	} catch (error) {
+		const { code, message } = error as AppwriteException;
+
+		return {
+			data: {
+				status: code,
+				message
+			}
+		};
+	}
+
+	return {
+		data: {
+			status: 200,
+			message: 'Email verified successfully!'
+		}
+	};
+}
---
+++ src/routes/verify-email/+page.svelte
@@ -0,0 +1,36 @@
+<script>
+	import { goto } from '$app/navigation';
+	import { PUBLIC_LANDING_IMAGE } from '$env/static/public';
+	import logo from '$lib/assets/logo.png';
+	import { Button } from '$lib/components/ui/button';
+
+	const { data } = $props();
+</script>
+
+<div class="flex h-screen w-screen items-center">
+	<img src={PUBLIC_LANDING_IMAGE} alt="landing" class="h-screen w-2/5 rounded-l-[50px] bg-cover" />
+
+	<div class="flex h-full w-full flex-col px-48">
+		<div class="flex w-full items-center justify-end py-12">
+			<Button href="/login" variant="ghost" class="mr-2 text-lg font-bold text-primary"
+				>Login</Button
+			>
+			<Button href="/register" variant="ghost" class="text-lg font-bold text-primary"
+				>Register</Button
+			>
+		</div>
+
+		<div class="flex items-center justify-start pb-28 pt-24">
+			<a href="/"><img src={logo} alt="logo" class="h-[100px] w-auto" /></a>
+		</div>
+
+		{#if data.data.status === 200}
+			<p class="mb-2 text-lg text-foreground">Your email has been successfully validated</p>
+			<p class="mb-3 text-base text-foreground">Click the button below to be redirected to login</p>
+
+			<Button class="my-8 w-48 font-bold" onclick={() => goto('/login')}>Login</Button>
+		{:else}
+			<p class="mb-2 text-lg text-foreground">{data.data.message}</p>
+		{/if}
+	</div>
+</div>
