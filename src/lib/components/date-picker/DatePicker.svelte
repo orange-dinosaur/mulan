066f1f3dc871274ca3b9bfbf5cb1ebb344fddf7f
orange-dinosaur <user@randomprojects.xyz>
2025-06-06 02:40:40 +0200

    Add month and year selection to date picker

--- src/lib/components/date-picker/DatePicker.svelte
+++ src/lib/components/date-picker/DatePicker.svelte
@@ -8,8 +8,10 @@
 		today
 	} from '@internationalized/date';
 	import { cn } from '$lib/utils.js';
-	import { Button, buttonVariants } from '$lib/components/ui/button/index.js';
-	import { Calendar } from '$lib/components/ui/calendar/index.js';
+	import { buttonVariants } from '$lib/components/ui/button/index.js';
+	import { Calendar as CalendarPrimitive } from 'bits-ui';
+	import * as Select from '$lib/components/ui/select/index.js';
+	import * as Calendar from '$lib/components/ui/calendar/index.js';
 	import * as Popover from '$lib/components/ui/popover/index.js';
 
 	let {
@@ -20,6 +22,9 @@
 	const df = new DateFormatter('it-IT', {
 		dateStyle: 'short'
 	});
+	const mf = new DateFormatter('en-IT', {
+		month: 'long'
+	});
 
 	let value = $state<DateValue | undefined>(
 		date?.compare(new CalendarDate(1970, 1, 1)) === 0 ||
@@ -27,17 +32,42 @@
 			? undefined
 			: date
 	);
+	let placeholder = $state<DateValue>();
 	let contentRef = $state<HTMLElement | null>(null);
+
+	const monthOptions = Array.from({ length: 12 }, (_, i) => {
+		const month = today(getLocalTimeZone()).set({ month: i + 1 });
+		return {
+			value: month.month,
+			label: mf.format(month.toDate(getLocalTimeZone()))
+		};
+	});
+
+	const yearOptions = Array.from({ length: 100 }, (_, i) => ({
+		label: String(new Date().getFullYear() - i),
+		value: new Date().getFullYear() - i
+	}));
+
+	const defaultYear = $derived(
+		placeholder ? { value: placeholder.year, label: String(placeholder.year) } : undefined
+	);
+
+	const defaultMonth = $derived(
+		placeholder
+			? {
+					value: placeholder.month,
+					label: mf.format(placeholder.toDate(getLocalTimeZone()))
+				}
+			: undefined
+	);
+
+	const monthLabel = $derived(
+		monthOptions.find((m) => m.value === defaultMonth?.value)?.label ?? 'Select a month'
+	);
 
 	function handleDateChange(date: DateValue) {
 		(onDateChange as (date: DateValue) => void)(date);
 	}
-
-	/* $effect(() => {
-		if (value) {
-			handleDateChange(value);
-		}
-	}); */
 </script>
 
 <div class="flex items-center gap-2">
@@ -56,7 +86,7 @@
 			{value ? df.format(value.toDate(getLocalTimeZone())) : 'Pick a date'}
 		</Popover.Trigger>
 		<Popover.Content bind:ref={contentRef} class="w-auto p-0 text-primary">
-			<Calendar
+			<!-- <Calendar
 				type="single"
 				bind:value
 				maxValue={today(getLocalTimeZone())}
@@ -65,7 +95,89 @@
 						handleDateChange(value);
 					}
 				}}
-			/>
+			/> -->
+
+			<CalendarPrimitive.Root
+				type="single"
+				weekdayFormat="short"
+				class={cn('rounded-md border p-3')}
+				bind:value
+				bind:placeholder
+				weekStartsOn={1}
+				maxValue={today(getLocalTimeZone())}
+				onValueChange={() => {
+					if (value) {
+						handleDateChange(value);
+					}
+				}}
+			>
+				{#snippet children({ months, weekdays })}
+					<Calendar.Header class="flex w-full items-center justify-between gap-2">
+						<Select.Root
+							type="single"
+							value={`${defaultMonth?.value}`}
+							onValueChange={(v) => {
+								if (!placeholder) return;
+								if (v === `${placeholder.month}`) return;
+								placeholder = placeholder.set({ month: Number.parseInt(v) });
+							}}
+						>
+							<Select.Trigger aria-label="Select month" class="w-[60%]">
+								{monthLabel}
+							</Select.Trigger>
+							<Select.Content class="max-h-[200px] overflow-y-auto">
+								{#each monthOptions as { value, label } (value)}
+									<Select.Item value={`${value}`} {label} />
+								{/each}
+							</Select.Content>
+						</Select.Root>
+						<Select.Root
+							type="single"
+							value={`${defaultYear?.value}`}
+							onValueChange={(v) => {
+								if (!v || !placeholder) return;
+								if (v === `${placeholder?.year}`) return;
+								placeholder = placeholder.set({ year: Number.parseInt(v) });
+							}}
+						>
+							<Select.Trigger aria-label="Select year" class="w-[40%]">
+								{defaultYear?.label ?? 'Select year'}
+							</Select.Trigger>
+							<Select.Content class="max-h-[200px] overflow-y-auto">
+								{#each yearOptions as { value, label } (value)}
+									<Select.Item value={`${value}`} {label} />
+								{/each}
+							</Select.Content>
+						</Select.Root>
+					</Calendar.Header>
+					<Calendar.Months>
+						{#each months as month (month)}
+							<Calendar.Grid>
+								<Calendar.GridHead>
+									<Calendar.GridRow class="flex">
+										{#each weekdays as weekday (weekday)}
+											<Calendar.HeadCell>
+												{weekday.slice(0, 2)}
+											</Calendar.HeadCell>
+										{/each}
+									</Calendar.GridRow>
+								</Calendar.GridHead>
+								<Calendar.GridBody>
+									{#each month.weeks as weekDates (weekDates)}
+										<Calendar.GridRow class="mt-2 w-full">
+											{#each weekDates as date (date)}
+												<Calendar.Cell class="select-none" {date} month={month.value}>
+													<Calendar.Day />
+												</Calendar.Cell>
+											{/each}
+										</Calendar.GridRow>
+									{/each}
+								</Calendar.GridBody>
+							</Calendar.Grid>
+						{/each}
+					</Calendar.Months>
+				{/snippet}
+			</CalendarPrimitive.Root>
 		</Popover.Content>
 	</Popover.Root>
 
@@ -77,6 +189,7 @@
 			onclick={() => {
 				value = undefined;
 				handleDateChange(new CalendarDate(0, 0, 0));
+				placeholder = undefined;
 			}}
 		>
 			x
